e7a4b72aa1dfb62e879bb2ca1f40ca2a56dd2246
Yuchen Huang <yuchen_huang@brown.edu>
2017-09-16 17:59:35 -0400

    css add

---
+++ src/js/home.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Router, Route, BrowserRouter } from 'react-router-dom';
+
+// import Layout from './pages/Layout';
+// import FridgeContent from './components/fridge_content';
+
+// const app = document.getElementById('app');
+
+// ReactDOM.render(
+// 	<BrowserRouter>
+// 	    <Route path="/home" component={FridgeContent} />
+//   	</BrowserRouter>,
+// app);
