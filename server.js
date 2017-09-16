c130f43159a710d07c47143691f8f4c03f983f5b
Yuchen Huang <yuchen_huang@brown.edu>
2017-09-16 15:40:26 -0400

    Add server.js

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 var http = require('http');
+var express = require('express');
+var app = express();
+var server = http.createServer(app);
+
+app.use(express.static(__dirname + '/src'));
+
+app.get('/', function(request, response){
+    response.render('index.html');
+});
+
+server.listen(8080, function(){
+    console.log('- Server listening on port 8080');
+});
 
