148b10cb20abafd3ca8c4ef0f66b3ef50d25d726
Linda Chang <linda_chang@brown.edu>
2017-09-16 21:07:05 -0400

    idk

--- server.js
+++ server.js
@@ -20,6 +20,24 @@ app.post('/image', function(req, res) {
 	var image = req.body.image;
 
 	console.log(image);
+
+	var spawn = require('child_process').spawn,
+    py = spawn('python', ['compute_input.py']),
+    data = [1,2,3,4,5,6,7,8,9],
+    dataString = '';
+
+    py.stdout.on('data', function(data){
+	  dataString += data.toString();
+	});
+
+	/*Once the stream is done (on 'end') we want to simply log the received data to the console.*/
+	py.stdout.on('end', function(){
+	  console.log('Sum of numbers=',dataString);
+	});
+
+	py.stdin.write(JSON.stringify(data));
+
+	py.stdin.end();
 });
 
 server.listen(8080, function(){
